c0e9ed0ebf0b339ba76d0c15bf6b03c32b18706a
HyeonJeong <58041212+HyeonJeong519@users.noreply.github.com>
2021-10-04 23:08:16 +0900

    Add files via upload

---
+++ 황현정/211005/Recursive/src/EuclidGCD.java
@@ -0,0 +1,23 @@
+import java.security.PublicKey;
+import java.util.*;
+
+public class EuclidGCD {
+    static int gcd(int x, int y) {
+        if (y == 0)
+            return x;
+        else
+            return gcd(x, x % y);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("두 정수의 최대공약수를 구합니다.");
+        System.out.println("정수를 입력하세요. : ");
+        int x = scan.nextInt();
+        System.out.println("정수를 입력하세요. : ");
+        int y = scan.nextInt();
+
+        System.out.println("최대공약수는 " + gcd(x, y) + "입니다.");
+    }
+}
